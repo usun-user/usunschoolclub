674f49da0256ec9972782219732db0efa3f9aba6
usun <usun@tut.by>
2025-04-05 21:06:55 -0400

    Made coding contest problem 3 faster, but website still rejects.

---
+++ src/edu/usun/schoolclub/DeckFlipping2.java
@@ -0,0 +1,32 @@
+package edu.usun.schoolclub;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class DeckFlipping2 {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(new FileInputStream(
+				"C:\\_igor\\eclipseworkspace\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"
+		));
+		int numberOfCards = sc.nextInt();
+		int numberOfLines = sc.nextInt();
+		int numberOfCardsUp = numberOfCards;
+		boolean[] arr = new boolean[numberOfCards];
+		for (int i = 0; i < numberOfLines; i++) {
+			int start = sc.nextInt();
+			for (int end = sc.nextInt(); start <= end; start++) {
+				boolean switchTo = !arr[start - 1];
+				arr[start-1] = switchTo;
+				if (switchTo) {
+					numberOfCardsUp--;
+				} else {
+					numberOfCardsUp++;
+				}
+			}	
+		}
+		System.out.println(numberOfCardsUp);
+		sc.close();
+	}
+
+}
