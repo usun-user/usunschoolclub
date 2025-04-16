0eadd2256743a28fd7ad26499551548f8c5294ad
igors <igors@Sha_1.EDU.YRDSB.NET>
2025-04-16 19:17:31 -0400

    Finished MagicSquares.

--- src/edu/usun/schoolclub/MagicSquares.java
+++ src/edu/usun/schoolclub/MagicSquares.java
@@ -7,41 +7,38 @@ public class MagicSquares {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new FileInputStream(
-				"C:\\_igor\\eclipseworkspace\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"
-		));
-		
-		/*
-		while (sc.hasNextLine()) {
-			String line = sc.nextLine();
-			System.out.println("Read line: " + line);
+				"C:\\_igor\\eclipseworkspace\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"));
+		int[][] magicSquare = new int[4][4];
+		int value = 0;
+		for (int i = 0; i < 4; i++) {
+			int currentValue = 0;
+			for (int j = 0; j < 4; j++) {
+				magicSquare[i][j] = sc.nextInt();
+				currentValue += magicSquare[i][j];
+			}
+			if (currentValue != value) {
+				if (i == 0) {
+					value = currentValue;
+				} else {
+					System.out.println("not magic");
+					sc.close();
+					return;
+				}
+			}
 		}
+		for (int i = 0; i < 4; i++) {
+			int currentValue = 0;
+			for (int j = 0; j < 4; j++) {
+				currentValue += magicSquare[j][i];
+			}
+			if (currentValue != value) {
+				System.out.println("not magic");
+				sc.close();
+				return;
+			}
+		}
+		System.out.println("magic");	
 		sc.close();
-		*/
-		
-		int[][] matrix = {
-				{1, 2, 3},
-				{4, 5, 6},
-				{7, 8, 9}			
-			};
-			
-			//accessing item at certain position
-			//System.out.println(matrix[1][1]);
-			
-			/*
-			for (int i = 0; i < matrix.length; i++) {
-				for (int j = 0; j < matrix[i].length; j++) {
-					System.out.print(matrix[i][j] + " ");
-				}
-				System.out.println();
-			}
-			*/
-			
-			for (int i = 0; i < matrix.length; i++) {
-				for (int j = 0; j < matrix[i].length; j++) {
-					System.out.print(matrix[i][j] + " ");
-				}
-				System.out.println();
-			}
 	}
 
 }
