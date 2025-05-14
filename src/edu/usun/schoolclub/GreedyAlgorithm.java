0ebeca8c28a71e75217baac8dee998f51bcb6350
igors <igors@Sha_1.EDU.YRDSB.NET>
2025-05-14 19:47:04 -0400

    Learned Greedy Algorithms.

---
+++ src/edu/usun/schoolclub/GreedyAlgorithm.java
@@ -0,0 +1,64 @@
+package edu.usun.schoolclub;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class GreedyAlgorithm {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(new FileInputStream(
+				"C:\\_igor\\eclipseworkspace\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"));
+		
+		// Greedy Algorithm example 1: Minimum number of coins (take largest coin each time)
+		/*
+		int[] coinsArray = {25, 10, 5, 1};
+		int target = 30;
+		int numberOfCoins = 0;
+		
+		while (target > 0) {
+			for (int j = 0; j < coinsArray.length; j++) {
+				if (coinsArray[j] <= target) {
+					numberOfCoins++;
+					target -= coinsArray[j];
+					break;
+				}
+			}
+		}
+		
+		System.out.println(numberOfCoins);
+		sc.close();
+		*/
+		
+		// Greedy Algorithm example: Minimum number of activities (always pick smallest finish time)
+		// My way only works with unique finish times because of HashMap
+		
+		int[] start = {1, 3, 0, 5, 8, 5};
+		int[] finish = {2, 4, 6, 7, 9, 10};
+		int currentTime = 0;
+		int numberOfActivities = 0;
+		
+		HashMap<Integer, Integer> activitiesMap = new HashMap<Integer, Integer>();
+		
+		int lengthOfArray = finish.length;
+		
+		for (int i = 0; i < lengthOfArray; i++) {
+			activitiesMap.put(finish[i], start[i]);
+		}
+		
+		Arrays.sort(finish);
+		
+		for (int i = 0; i < lengthOfArray; i++) {
+			if (activitiesMap.get(finish[i]) >= currentTime) {
+				numberOfActivities++;
+				currentTime = finish[i];
+			}
+		}
+		
+		System.out.println(numberOfActivities);
+		sc.close();
+		
+	}
+
+}
