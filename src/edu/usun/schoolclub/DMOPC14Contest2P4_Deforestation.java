2432fb9744a979892a85466feb2691aa9606e6dc
igors <igors@Sha_1.EDU.YRDSB.NET>
2025-09-23 13:39:01 -0400

    Finished DMOPC 14 Contest 2 P4 - Deforestation problem.

--- src/edu/usun/schoolclub/DMOPC14Contest2P4_Deforestation.java
+++ src/edu/usun/schoolclub/DMOPC14Contest2P4_Deforestation.java
@@ -8,22 +8,21 @@ public class DMOPC14Contest2P4_Deforestation {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new FileInputStream(
 				"C:\\_igor\\eclipseworkspace\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"));
-		//int x = 10;
-		//String mySentence = "hello world";
-		//boolean tired = true;
-		//float myFloat = 5.6f;
-		
 		int numberOfTrees = sc.nextInt();
-		int[] arrayOfTrees = new int[numberOfTrees];
+		int[] arrayOfMasses = new int[numberOfTrees];
+		int[] massPSA = new int[numberOfTrees + 1];
 		for (int i = 0; i < numberOfTrees; i++) {
-			arrayOfTrees[numberOfTrees] = sc.nextInt();
+			arrayOfMasses[i] = sc.nextInt();
+			massPSA[i + 1] = arrayOfMasses[i] + massPSA[i];
 		}
 		int numberOfQueries = sc.nextInt();
 		for (int j = 0; j < numberOfQueries; j++) {
-			
+			int lowRange = sc.nextInt();
+			int highRange = sc.nextInt();
+			int mass = massPSA[highRange + 1] - massPSA[lowRange];
+			// using PSA (Prefix Sum Array) add the highRange+1 value and subtract the lowRange value.
+			System.out.println(mass);
 		}
-		
-		//System.out.println(x + mySentence + tired + myFloat);
 		sc.close();
 	}
 
