7064fd36be7898e235facd57e369d96d759436b7
igors <igors@Sha_1>
2025-10-19 17:59:49 -0400

    Finished DMOJ Age Demographic.

--- src/edu/usun/schoolclub/DMOJ_AgeDemographic.java
+++ src/edu/usun/schoolclub/DMOJ_AgeDemographic.java
@@ -1,29 +1,65 @@
 package edu.usun.schoolclub;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+//import java.io.InputStreamReader;
 
 public class DMOJ_AgeDemographic {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner sc = new Scanner(new FileInputStream(
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader(
 				"C:\\_igor\\eclipseworkspace\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"));
-		int numberOfViewers = sc.nextInt();
+		int numberOfViewers = Integer.parseInt(br.readLine());
 		int[] ages = new int[numberOfViewers];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < numberOfViewers; i++) {
-			ages[i] = sc.nextInt();
+			ages[i] = Integer.parseInt(st.nextToken());
 		}
-		int numberOfLines = sc.nextInt();
-		
+		Arrays.sort(ages);		
+		int numberOfLines = Integer.parseInt(br.readLine());
 		for (int i = 0; i < numberOfLines; i++) {
-			int low = sc.nextInt();
-			int high = sc.nextInt();
-			// ?
+			st = new StringTokenizer(br.readLine());
+			int targetLow = Integer.parseInt(st.nextToken());
+			int targetHigh = Integer.parseInt(st.nextToken());
+			int lowAns = 0;
+			int highAns = numberOfViewers;
+			if (targetHigh < ages[0]) {
+				System.out.println(0);
+				continue;
+			} else if (targetHigh < ages[numberOfViewers - 1]) {
+				int low = 0;
+				int high = numberOfViewers - 1;
+				while (low <= high) {
+					int mid = (high + low) / 2;
+					if (ages[mid] <= targetHigh) {
+						low = mid + 1;
+					} else {
+						highAns = mid;
+						high = mid - 1;
+					}
+				}
+			}
+			if (targetLow > ages[numberOfViewers - 1]) {
+				System.out.println(0);
+				continue;
+			} else if (targetLow > ages[0]) {
+				int low = 0;
+				int high = highAns;
+				while (low <= high) {
+					int mid = (high + low) / 2;
+					if (ages[mid] >= targetLow) {
+						lowAns = mid;
+						high = mid - 1;
+					} else {
+						low = mid + 1;
+					}
+				}
+			}
+			System.out.println(highAns - lowAns);
 		}
-		
-		sc.close();
+		br.close();
 	}
-
 
 }
